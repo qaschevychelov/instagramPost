87b75e14a1c3bd3f2214ff1713543034c91008d4
qaschevychelov <qa.schevychelov@gmail.com>
2020-02-11 00:17:53 +0300

    поправил капабилити

--- src/test/java/BasePage.java
+++ src/test/java/BasePage.java
@@ -14,11 +14,11 @@ public class BasePage {
         cap.setCapability("platformVersion", "8.1");
         cap.setCapability("app", System.getProperty("user.dir") + "\\src\\test\\resources\\apk\\instagram.apk");
         cap.setCapability("appPackage", "com.instagram.android");
-        cap.setCapability("appActivity", "com.instagram.nux.activity.SignedOutFragmentActivity");
+        cap.setCapability("appWaitActivity", "com.instagram.nux.activity.SignedOutFragmentActivity");
         cap.setCapability("noReset", "true");
         cap.setCapability("fullReset", "false");
         cap.setCapability("newCommandTimeout", "30000");
-        cap.setCapability("automationName", "Espresso");
+        cap.setCapability("automationName", "UiAutomator2");
         AndroidDriver androidDriver = null;
         try {
             System.out.println("starting new driver..." + LocalDateTime.now().toString());
